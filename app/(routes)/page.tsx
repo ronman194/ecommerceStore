caa38756e2231dfb9c8f6b87c4202b1b3f0d6923
FinalProjectRonMaor <139042047+FinalProjectRonMaor@users.noreply.github.com>
2023-08-07 15:19:29 +0300

    add navbar, home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,25 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+    const billboard = await getBillboard("4a3f7c3d-de00-4b54-99b8-02a5259f3ce9");
+    const products = await getProducts({ isFeatured: true });
     return (
-        <div>
-            Hello Store
-        </div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+
+        </Container>
     );
 }
 
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs">
+                    &copy; 2023 BestStoreEver, Inc. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
